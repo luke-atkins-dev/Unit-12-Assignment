d1b43445c5c2169e121e4c4299e1ce2f802358b8
aaron <aaron>
2026-04-07 23:46:02 -0400

    Added init for bullet

--- bullet.py
+++ bullet.py
@@ -9,6 +9,18 @@ if TYPE_CHECKING:
 class Bullet(Sprite):
     def __init__(self, game: "AlienInvasion") -> None:
         super().__init__()
+        self.game = game
         self.screen = game.screen
         self.settings = game.settings
-        
+
+        self.image = pygame.image.load(
+            self.settings.bullet_file
+        )
+        self.image = pygame.transform.scale(
+            self.image,
+            (self.settings.bullet_w, self.settings.bullet_h)
+        )
+
+        self.rect = self.image.get_rect()
+        self.rect.midtop = game.ship.rect.midtop
+        self.y = float(self.rect.y)
