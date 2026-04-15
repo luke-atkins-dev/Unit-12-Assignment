9e8dd43cb6fa139c103ab76bac897de63a32b977
aaron <aaron>
2026-04-15 19:56:06 -0400

    Made alien drop when reaching the side of the screen

--- Settings.py
+++ Settings.py
@@ -40,5 +40,6 @@ class Settings:
         self.alien_file = project / "Assets" / "images" / "enemy_4.png"
         self.alien_w = 40
         self.alien_h = 40
-        self.fleet_speed = 2
+        self.fleet_speed = 8
         self.fleet_direction = 1
+        self.fleet_drop_speed = 40
